c22ee4d2fb3df8b49f04b4e1983e2db505412da0
Taki <tarek.laib24@gmail.com>
2026-06-13 16:09:23 +0100

    quine in c is done

--- quine.c
+++ quine.c
@@ -32,7 +32,9 @@ void printSelf(char *s) {
 
 int main(void) {
 
-  char *src = "\n#include <stdio.h>\n\nvoid printEscaped(char *s) {\n  for (int i = 0; s[i]; i++) {\n    switch (s[i]) {\n    case '\n':\n      putchar('\\');\n      putchar('n');\n      continue;\n    case '\"':\n      putchar('\\');\n      putchar('\"');\n      continue;\n    case '\\':\n      putchar('\\');\n      putchar('\\');\n      continue;\n    default:\n      putchar(s[i]);\n    };\n  }\n}\n\nvoid printSelf(char *s) {\n  for (int i = 0; s[i]; i++) {\n    if (s[i] == 33)\n      printEscaped(s);\n    else\n      putchar(s[i]);\n  }\n}\n\nint main(void) {\n\n  char *src = \"!\" ;\n  printSelf(src);\n  return 0;\n}\n";
+  char *src = "\n#include <stdio.h>\n\nvoid printEscaped(char *s) {\n  for (int i = 0; s[i]; i++) {\n    switch (s[i]) {\n    case '\\n':\n      putchar('\\');\n      putchar('n');\n      continue;\n    case '\"':\n      putchar('\\');\n      putchar('\"');\n      continue;\n    case '\\':\n      putchar('\\');\n      putchar('\\');\n      continue;\n    default:\n      putchar(s[i]);\n    };\n  }\n}\n\nvoid printSelf(char *s) {\n  for (int i = 0; s[i]; i++) {\n    if (s[i] == 33)\n      printEscaped(s);\n    else\n      putchar(s[i]);\n  }\n}\n\nint main(void) {\n\n  char *src = \"!\";\n\n  printSelf(src);\n\n  return 0;\n}\n";
+
   printSelf(src);
+
   return 0;
 }
