cd25164ce7c1d3fdcc5b58fb69a5286a33291c74
Taki <tarek.laib24@gmail.com>
2026-06-14 11:53:03 +0100

    a quine in python that actually works

--- quine.py
+++ quine.py
@@ -1,9 +1,2 @@
-def printSelf(s):
-    for c in s:
-        if c == chr(63):
-            print(repr(s), end='')
-        else:
-            print(c, end='')
-
-self = """def printSelf(s):\n    for c in s:\n        if c == chr(63):\n            print(repr(s), end='')\n        else:\n            print(c, end='')\n\nself = ?\nprintSelf(self)\n"""
-printSelf(self)
+s='s=%r\nprint(s%%s)'
+print(s%s)
